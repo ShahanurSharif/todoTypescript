4cb6e0787a48d62515a0181b907e2ad1a8ced566
sharif <shahanur700@gmail.com>
2025-05-19 02:02:30 +0800

    task

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,28 @@ type Task = {
 }
 
 function App() {
-    const tasks: Task[] = [{id:1, title:'learn react', isCompleted: false}]
+    const [tasks, setTasks] = useState<Task[]>([{id: 1, title: 'learn react', isCompleted: false}])
     const [taskName, setTaskName] = useState('')
+    const onAddTask = () => {
+        setTasks([
+            ...tasks,
+            {id: Date.now(), title: taskName, isCompleted: false}
+        ])
+    }
     return (
         <div>
             <h1>Tasks</h1>
             <label htmlFor="task-input">add task:</label>
-            <input type="text" id="task-input" value={taskName} onChange={(e)=>setTaskName(e.target.value)}/>
-            <button onClick={()=>console.log(taskName)}>Add</button>
+            <input type="text" 
+                   id="task-input" 
+                   value={taskName} 
+                   onChange={(e) => setTaskName(e.target.value)}
+            />
+            <button onClick={onAddTask}>Add</button>
             <ul>
-                {tasks.map(task => <li key={task.id}>{task.title}</li>)}
+                {tasks.map((task)=>(
+                    <li key={task.id}>{task.title}</li>
+                ))}
             </ul>
         </div>
     )
